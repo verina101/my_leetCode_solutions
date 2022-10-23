845343102f48cae8a08b7c558bc8ea9d8a83668b
verina101 <91009011+verina101@users.noreply.github.com>
2022-10-23 20:20:10 +0200

    Time: 476 ms (18.69%), Space: 45.4 MB (38.22%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    map<string, vector<string>>adj;
+    vector<vector<string>> ans;
+    set<string>vis;
+    
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        for(auto &acc:accounts){
+            for(int i=1;i<acc.size()-1;i++){
+                adj[acc[i]].push_back(acc[i+1]);
+                adj[acc[i+1]].push_back(acc[i]);
+            }
+        }
+        
+        for(auto& acc: accounts){
+            if(!vis.count(acc[1])){
+                ans.push_back({acc[0]});
+                dfs(acc[1]);
+                sort(ans.back().begin()+1, ans.back().end());
+            }
+        }
+        return ans;
+    }
+    
+    void dfs(string& email){
+        vis.insert(email);
+        ans.back().push_back(email);
+        for(auto& e : adj[email]){
+            if(!vis.count(e)){
+                dfs(e);
+            }
+        }
+    }
+};
