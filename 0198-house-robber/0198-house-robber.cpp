217bc849f5750028c53c05396438e974c40fee03
verina101 <91009011+verina101@users.noreply.github.com>
2022-10-29 01:00:25 +0200

    Time: 3 ms (56.84%), Space: 7.8 MB (49.80%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        dp[0]=0;
-        dp[1] = nums[0];  
-
+        vector<int>dp(nums.size(),-1);
+        dp[0] = nums[0];  
+        // dp[1] = max(dp[0],nums[1]);
         for(int i=1;i<nums.size();i++){
-           dp[i+1] = max(dp[i],nums[i]+dp[i-1]);  
+           dp[i] = max(dp[i-1],nums[i]+ (i-2>=0 ? dp[i-2]:0));
+            
         }
-        return dp[nums.size()];
+        return dp[nums.size()-1];
     }
 };
